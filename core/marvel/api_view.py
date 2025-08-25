662f9c0c2cb7fcc415772b738e45f906435272df
Bibek Shah <124855267+Bibekshah123@users.noreply.github.com>
2025-08-25 21:48:11 +0545

    added token authentication

--- core/marvel/api_view.py
+++ core/marvel/api_view.py
@@ -1,10 +1,14 @@
 from rest_framework import generics, permissions
 from .models import Superhero
-from .serializers import SuperheroSerializer
+from .serializers import *
 from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth.models import User
 
 
-
+class UserSignupView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSignupSerializer
+    permission_classes = [permissions.AllowAny]
 
 # List + Create (GET all, POST new)
 class SuperheroListCreateAPIView(generics.ListCreateAPIView):
--- core/marvel/serializers.py
+++ core/marvel/serializers.py
@@ -1,7 +1,27 @@
 from rest_framework import serializers
 from .models import Superhero
+from django.contrib.auth.models import User
+
 
 class SuperheroSerializer(serializers.ModelSerializer):
     class Meta:
         model = Superhero
         fields = '__all__'
+
+class UserSignupSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
+    password2 = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'password2']
+
+    def validate(self, data):
+        if data['password'] != data['password2']:
+            raise serializers.ValidationError("Passwords do not match.")
+        return data
+
+    def create(self, validated_data):
+        validated_data.pop('password2')
+        user = User.objects.create_user(**validated_data)
+        return user
